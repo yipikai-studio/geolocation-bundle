32a5df14ffb7d74e9785402dd4488d7038379a17
Matthieu Beurel <matthieu@mbeurel.com>
2024-07-15 16:19:49 +0200

    feat : Update Symfony version to 6.4

--- YipikaiGeolocationBundle.php
+++ YipikaiGeolocationBundle.php
@@ -22,7 +22,7 @@ class YipikaiGeolocationBundle extends Bundle
   /**
    * @param ContainerBuilder $container
    */
-  public function build(ContainerBuilder $container)
+  public function build(ContainerBuilder $container): void
   {
     parent::build($container);
   }
